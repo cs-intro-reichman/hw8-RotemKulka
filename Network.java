87b8f8427b65b0a31a9d60e88d50e770d3037242
RotemKulka <rotem.kulka@post.runi.ac.il>
2026-01-11 20:55:40 +0200

    Update Network.java

--- Network.java
+++ Network.java
@@ -56,7 +56,9 @@ public class Network {
     public boolean addFollowee(String name1, String name2) {
         User user1 = getUser(name1);
         User user2 = getUser(name2);
-        if (name1==null || name2==null || name1.equals(name2)) return false;
+        if (user1==null || user2==null || name1.equals(name2)){
+            return false;
+        }
         return user1.addFollowee(name2);
     }
     
